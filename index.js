68788ded47b2be7d04e31b885baf43bf2c1099ad
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-31 14:22:38 -0700

    added dynamic port selection

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const server = require('./server.js');
 
+const port = process.env.PORT || 9000;
 
-
-server.listen(4000, () => {
+server.listen(port, () => {
     console.log('\n* Server Running on http://localhost:4000 *\n');
 });
